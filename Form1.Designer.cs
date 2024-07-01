913018a73f8c16776849c7826736432aed678788
Nguyen Cuong <wazza.cuong@gmail.com>
2024-07-01 16:32:39 +0700

    feat: create app run cmd analysis picture

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Software_analysis_picture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TestItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Command = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox_result_output = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.TestItem,
+            this.Command});
+            this.dataGridView1.Location = new System.Drawing.Point(53, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(1157, 243);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.MinimumWidth = 8;
+            this.No.Name = "No";
+            this.No.ReadOnly = true;
+            this.No.Width = 40;
+            // 
+            // TestItem
+            // 
+            this.TestItem.HeaderText = "TestItem";
+            this.TestItem.MinimumWidth = 8;
+            this.TestItem.Name = "TestItem";
+            this.TestItem.ReadOnly = true;
+            this.TestItem.Width = 400;
+            // 
+            // Command
+            // 
+            this.Command.HeaderText = "Command";
+            this.Command.MinimumWidth = 8;
+            this.Command.Name = "Command";
+            this.Command.ReadOnly = true;
+            this.Command.Width = 150;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 26);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(1051, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Station";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox_result_output
+            // 
+            this.richTextBox_result_output.BackColor = System.Drawing.Color.Black;
+            this.richTextBox_result_output.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_result_output.ForeColor = System.Drawing.Color.White;
+            this.richTextBox_result_output.Location = new System.Drawing.Point(53, 402);
+            this.richTextBox_result_output.Name = "richTextBox_result_output";
+            this.richTextBox_result_output.Size = new System.Drawing.Size(1157, 324);
+            this.richTextBox_result_output.TabIndex = 4;
+            this.richTextBox_result_output.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select Station:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 779);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox_result_output);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "VCS001 ANALYSIS PICTURE";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox_result_output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TestItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Command;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_analysis_picture
+{
+    public partial class Form1 : Form
+    {
+        string valueSelectCD = @"CD C:\VCS001\POT_image_test";
+        string nameStationSelected = "T2.3";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataFromFile();
+            LoadComboBoxOptions();
+        }
+
+        private void LoadDataFromFile()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{nameStationSelected}.txt");
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("File not found!");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            dataGridView1.Rows.Clear();
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 3)
+                {
+                    dataGridView1.Rows.Add(parts[0], parts[1], parts[2], "RUN");
+                }
+            }
+
+            DataGridViewButtonColumn runButtonColumn = new DataGridViewButtonColumn();
+            runButtonColumn.Name = "Action";
+            runButtonColumn.Text = "RUN";
+            runButtonColumn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(runButtonColumn);
+
+           dataGridView1.CellClick += DataGridView1_CellClick;
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Action"].Index && e.RowIndex >= 0)
+            {
+                string executablePath = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                //MessageBox.Show("value RUN: " +  executablePath + " - CD: " + valueSelectCD);
+                RunCommandInCmd(valueSelectCD, executablePath);
+            }
+        }
+
+        private void LoadComboBoxOptions()
+        {
+            string comboBoxFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "options.txt");
+            if (!File.Exists(comboBoxFilePath))
+            {
+                MessageBox.Show("Options file not found!");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(comboBoxFilePath);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 3)
+                {
+                    comboBox1.Items.Add(new ComboBoxItem(parts[1], parts[2]));
+                }
+            }
+
+            comboBox1.DisplayMember = "Label";
+            comboBox1.ValueMember = "Value";
+
+            // Chọn mục đầu tiên trong ComboBox nếu có dữ liệu
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBoxItem selectedItem = comboBox1.SelectedItem as ComboBoxItem;
+            if (selectedItem != null)
+            {
+                valueSelectCD = selectedItem.Value;
+                nameStationSelected = selectedItem.Label;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{nameStationSelected}.txt");
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("File not found!");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            dataGridView1.Rows.Clear();
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 3)
+                {
+                    dataGridView1.Rows.Add(parts[0], parts[1], parts[2], "RUN");
+                }
+            }
+        }
+
+        private void RunCommandInCmd(string CD, string command)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = $"cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.CreateNoWindow = true;//hidden
+            p.StartInfo.RedirectStandardError = true;
+            p.Start();
+            p.StandardInput.WriteLine(@"C:");
+            p.StandardInput.Flush();
+            p.StandardInput.WriteLine($"{CD}");
+            p.StandardInput.Flush();
+            p.StandardOutput.DiscardBufferedData();
+            p.StandardInput.WriteLine($"{command}");
+            p.StandardInput.Flush();
+            p.StandardInput.Close();
+
+            // Đọc và hiển thị kết quả
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+
+            p.WaitForExit();
+
+            int index = output.IndexOf(command);
+            if (index != -1)
+            {
+                string result = output.Substring(index);
+                richTextBox_result_output.Text = "RUN ==> " + result;
+            }
+            else
+            {
+                MessageBox.Show("Command not found in the result string.");
+            }
+
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                MessageBox.Show("Error ==> ", error);
+            }
+        }
+    }
+
+    public class ComboBoxItem
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+
+        public ComboBoxItem(string label, string value)
+        {
+            Label = label;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Label;
+        }
+    }
+}
